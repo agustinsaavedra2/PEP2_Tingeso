0c300de57e2fb76dd57dc9faf987f3d060fbde15
agustinsaavedra2 <agustin.saavedra@usach.cl>
2025-05-13 16:30:14 -0400

    Conexión entre config-service y eureka-service arreglado.

---
+++ microservicios/booking-rate-service/src/main/java/com/example/booking_rate_service/BookingRateServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.booking_rate_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingRateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingRateServiceApplication.class, args);
+	}
+
+}
---
+++ microservicios/booking-rate-service/src/test/java/com/example/booking_rate_service/BookingRateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.booking_rate_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingRateServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
